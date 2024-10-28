743aaf94129866add61f2685c73592ebcb8fc482
Abs D <ipabhi@gmail.com>
2024-10-28 19:56:16 +0530

    update

--- src/pages/Components/GdpChart.js
+++ src/pages/Components/GdpChart.js
@@ -1,76 +1,71 @@
-import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
-const GdpChart = ({ gdpData }) => {
-  // console.log('GDP Data:', gdpData);
+const GdpChart = ({gdpData}) => {
+	// console.log('GDP Data:', gdpData);
 
-  // Check data structure
-  if (!gdpData || !gdpData.India) {
-    return <div>No data available</div>;
-  }
+	// Check data structure
+	if (!gdpData || !gdpData.India) {
+		return <div>No data available</div>;
+	}
 
-  // Prepare data for Highcharts
-  const year = gdpData.India.map((data) => data.year).reverse();
-  // console.log('Years for x-axis:', years);
-  const indiaData = gdpData.India.map((data) =>
-    // Limit to 2 decimal places
-    parseFloat(data.value.toFixed(2))
-  ).reverse();
-  const chinaData = gdpData.China.map((data) =>
-    parseFloat(data.value.toFixed(2))
-  ).reverse();
-  const usaData = gdpData.USA.map((data) =>
-    parseFloat(data.value.toFixed(2))
-  ).reverse();
+	// Prepare data for Highcharts
+	const year = gdpData.India.map((data) => data.year).reverse();
+	// console.log('Years for x-axis:', years);
+	const indiaData = gdpData.India.map((data) =>
+		// Limit to 2 decimal places
+		parseFloat(data.value.toFixed(2))
+	).reverse();
+	const chinaData = gdpData.China.map((data) => parseFloat(data.value.toFixed(2))).reverse();
+	const usaData = gdpData.USA.map((data) => parseFloat(data.value.toFixed(2))).reverse();
 
-  // Highcharts options
-  const options = {
-    chart: {
-      type: 'area',
-    },
-    title: {
-      text: '',
-    },
-    xAxis: {
-      categories: year, // Display year value
-      title: {
-        text: 'Year',
-      },
-    },
-    yAxis: {
-      title: {
-        text: 'GDP Growth (%)',
-      },
-      min: 0,
-    },
-    series: [
-      {
-        name: 'India',
-        data: indiaData,
-      },
-      {
-        name: 'China',
-        data: chinaData,
-      },
-      {
-        name: 'USA',
-        data: usaData,
-      },
-    ],
-    plotOptions: {
-      area: {
-        marker: {
-          enabled: false,
-        },
-        dataLabels: {
-          enabled: false,
-        },
-      },
-    },
-  };
+	// Highcharts options
+	const options = {
+		chart: {
+			type: 'area',
+		},
+		title: {
+			text: '',
+		},
+		xAxis: {
+			categories: year, // Display year value
+			title: {
+				text: 'Year',
+			},
+		},
+		yAxis: {
+			title: {
+				text: 'GDP Growth (%)',
+			},
+			min: 0,
+		},
+		series: [
+			{
+				name: 'India',
+				data: indiaData,
+			},
+			{
+				name: 'China',
+				data: chinaData,
+			},
+			{
+				name: 'USA',
+				data: usaData,
+			},
+		],
+		plotOptions: {
+			area: {
+				marker: {
+					enabled: false,
+				},
+				dataLabels: {
+					enabled: false,
+				},
+			},
+		},
+	};
 
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+	return <HighchartsReact highcharts={Highcharts} options={options} />;
 };
 
 export default GdpChart;
--- src/pages/Components/GdpTable.js
+++ src/pages/Components/GdpTable.js
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import React, {useState, useEffect} from 'react';
+import {AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
-const GdpTable = ({ gdpData }) => {
-  const [rowData, setRowData] = useState([]);
+const GdpTable = ({gdpData}) => {
+	const [rowData, setRowData] = useState([]);
 
-  // Prepare column header for AG Grid table
-  const columnDefs = [
-    { headerName: 'Country', field: 'country', flex: 2, sortable: true },
-    {
-      headerName: 'Year',
-      field: 'year',
-      flex: 2,
-      sortable: true,
-      sort: 'desc',
-    },
-    {
-      headerName: 'GDP Growth (%)',
-      field: 'value',
-      flex: 2,
-      sortable: true,
-      width: 150,
-      valueFormatter: (params) => `${params.value.toFixed(2)}%`,
-    },
-  ];
+	// Prepare column header for AG Grid table
+	const columnDefs = [
+		{headerName: 'Country', field: 'country', flex: 2, sortable: true},
+		{
+			headerName: 'Year',
+			field: 'year',
+			flex: 2,
+			sortable: true,
+			sort: 'desc',
+		},
+		{
+			headerName: 'GDP Growth (%)',
+			field: 'value',
+			flex: 2,
+			sortable: true,
+			sort: 'desc',
+			width: 150,
+			valueFormatter: (params) => `${params.value.toFixed(2)}%`,
+		},
+	];
 
-  // Prepare row data for AG Grid table
-  useEffect(() => {
-    const formattedData = [];
-    Object.keys(gdpData).forEach((country) => {
-      gdpData[country].forEach((item) => {
-        formattedData.push({
-          country,
-          year: item.year,
-          value: item.value,
-        });
-      });
-    });
-    setRowData(formattedData);
-  }, [gdpData]);
+	// Prepare row data for AG Grid table
+	useEffect(() => {
+		const formattedData = [];
+		Object.keys(gdpData).forEach((country) => {
+			gdpData[country].forEach((item) => {
+				formattedData.push({
+					country,
+					year: item.year,
+					value: item.value,
+				});
+			});
+		});
+		setRowData(formattedData);
+	}, [gdpData]);
 
-  return (
-    <div
-      className='ag-theme-alpine'
-      style={{
-        height: 500,
-      }}
-    >
-      <AgGridReact
-        columnDefs={columnDefs}
-        rowData={rowData}
-        pagination={true}
-        paginationPageSize={10}
-        paginationPageSizeSelector={[10, 25]}
-      />
-    </div>
-  );
+	return (
+		<div
+			className='ag-theme-alpine'
+			style={{
+				height: 500,
+			}}>
+			<AgGridReact
+				columnDefs={columnDefs}
+				rowData={rowData}
+				pagination={true}
+				paginationPageSize={10}
+				paginationPageSizeSelector={[10, 25]}
+			/>
+		</div>
+	);
 };
 
 export default GdpTable;
